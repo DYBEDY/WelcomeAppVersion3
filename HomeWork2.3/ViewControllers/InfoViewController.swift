db0c37349ab9935f6c9909965fb73ec16082faeb
DYBEDY <rd.iosdeveloper@gmail.com>
2021-12-20 15:26:08 +0300

    final correction

--- HomeWork2.3/ViewControllers/InfoViewController.swift
+++ HomeWork2.3/ViewControllers/InfoViewController.swift
@@ -16,7 +16,7 @@ class InfoViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        title = user.person.name
         applyGradientForBackground()
         
 }
